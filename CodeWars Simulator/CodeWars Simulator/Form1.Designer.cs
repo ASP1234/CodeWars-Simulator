c71177dcbdc7a2aa35b9ba9d1d33bfa73c871e89
ASP1234 <nimish_singhal26@yahoo.com>
2015-10-07 13:25:39 +0530

    Add icon

--- CodeWars Simulator/CodeWars Simulator/Form1.Designer.cs
+++ CodeWars Simulator/CodeWars Simulator/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
             this.bonus_btn1 = new System.Windows.Forms.Button();
             this.neg_btn1 = new System.Windows.Forms.Button();
@@ -675,6 +676,7 @@
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "CODE WARS";
             this.panel1.ResumeLayout(false);
